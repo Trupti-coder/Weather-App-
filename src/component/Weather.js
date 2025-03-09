a98041bcbfd8ccadaee05a01d4953adeae968848
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-09 15:08:27 -0400

    Update Weather.js

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -5,9 +5,19 @@ function Weather(){
     const[weatherData,setWeatherData]=useState(null);
     const[city,setCity]=useState('Pune');
     const[unit,setUnit]=useState('metric');
-    
+
     return(
         <>
+        <div>
+            <input type="text"
+            value={city}
+            placeholder="Enter City"
+            />
+            <select>
+                <option value="metric">Celsius</option>
+                <option value="imperial">Fahrenheit</option>
+            </select>
+        </div>
         </>
     );
 }
