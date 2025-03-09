fc5b4912c822d4ea95e257de50eafd6af53a8b3a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-09 15:05:07 -0400

    Update Weather.js

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function Weather(){
+
+    const[weatherData,setWeatherData]=useState(null);
+    const[city,setCity]=useState('Pune');
+    const[unit,setUnit]=useState('metric');
+    
     return(
         <>
         </>
